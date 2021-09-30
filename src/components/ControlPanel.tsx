686b8887fd1690d8476348c22b06a5c60afc89b8
Sebastian Cortes <scortesz@udel.edu>
2021-09-30 19:21:42 -0400

    Changed button visual with bootstrap

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,10 +1,10 @@
-import Button from '@restart/ui/esm/Button';
-import { Col } from 'react-bootstrap';
+import { Button, Col } from 'react-bootstrap';
 import { Card } from '../interfaces/card'
 
 export function ControlPanel({setCard}: {setCard: (c: Card)=>void}): JSX.Element {
     return <Col>
-        <h2>Control Panel</h2>        <Button>Previous Card</Button>
+        <h2>Control Panel</h2>        
+        <Button>Previous Card</Button>
         <Button>Next Card</Button>
     </Col>
 }
